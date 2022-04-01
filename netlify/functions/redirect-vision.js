7e6f888a8e3219aac19e069bced81d252d2405ad
dongji0903 <35845323+dongji0903@users.noreply.github.com>
2022-04-01 12:21:01 +0900

    Update redirect-vision.js

--- netlify/functions/redirect-vision.js
+++ netlify/functions/redirect-vision.js
@@ -1,6 +1,28 @@
+const { jQuery } = require("jquery");
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + 'AIzaSyBKBL1YGMY0ZMv_P6mG_01kUvTYUDSUvyE';
 exports.handler = async function(event, context) {
-  return {
+    var request = {
+    requests: [{
+      image: {
+        content: event.body
+      },
+      features: [{
+        //type: type,
+        //maxResults: 200
+      }]
+    }]
+  };
+ const result =  await $.post({
+    url: CV_URL,
+    data: JSON.stringify(request),
+    contentType: 'application/json'
+  });
+  
+return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    headers: {
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+    },
+    body: JSON.stringify({ message: "Hello World" })
   };
 }
